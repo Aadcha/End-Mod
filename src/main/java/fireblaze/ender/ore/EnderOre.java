e6b97ef0d93f60209c741a19b9235b4eb916b283
Aadi Chauhan <emailaadichauhan@gmail.com>
2021-02-12 13:24:51 -0800

    added enderthyst ore generation

--- src/main/java/fireblaze/ender/ore/EnderOre.java
+++ src/main/java/fireblaze/ender/ore/EnderOre.java
@@ -1,15 +1,31 @@
 package fireblaze.ender.ore;
 
-import net.minecraft.util.registry.Registry;
-
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
+import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
+import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.structure.rule.BlockMatchRuleTest;
 import net.minecraft.util.Identifier;
-
+import net.minecraft.util.registry.BuiltinRegistries;
+import net.minecraft.util.registry.Registry;
+import net.minecraft.util.registry.RegistryKey;
+import net.minecraft.world.gen.decorator.Decorator;
+import net.minecraft.world.gen.decorator.RangeDecoratorConfig;
+import net.minecraft.world.gen.feature.ConfiguredFeature;
+import net.minecraft.world.gen.feature.Feature;
+import net.minecraft.world.gen.feature.OreFeatureConfig;
+import net.minecraft.world.gen.GenerationStep;
 public class EnderOre implements ModInitializer {
 
 	public static final Item ENDERTHYST_SHARD = new Item(new Item.Settings().group(ItemGroup.MATERIALS));
+	public static final Block ENDERTHYST_ORE = new EnderthystOreBlock(FabricBlockSettings.copy(Blocks.ANCIENT_DEBRIS));
+	private static ConfiguredFeature<?,?> ENDERTHYST_ORE_END = Feature.ORE.configure(new OreFeatureConfig(new BlockMatchRuleTest(Blocks.END_STONE), ENDERTHYST_ORE.getDefaultState(),9)).decorate(Decorator.RANGE.configure(new RangeDecoratorConfig(0,0,240))).spreadHorizontally().repeat(20);
+
 
 	@Override
 	public void onInitialize() {
@@ -40,5 +56,11 @@ public class EnderOre implements ModInitializer {
 		Registry.register(Registry.ITEM, new Identifier("enderthyst", "enderthyst_shovel"),  new  EnderthystShovel(new EnderthystToolMaterial(),  shovelAttackDamage,  shovelAttackSpeed, new Item.Settings().group(ItemGroup.TOOLS)));
 		Registry.register(Registry.ITEM, new Identifier("enderthyst", "enderthyst_hoe"),     new     EnderthystHoe(new EnderthystToolMaterial(),     hoeAttackDamage,     hoeAttackSpeed, new Item.Settings().group(ItemGroup.TOOLS)));
 
+		Registry.register(Registry.BLOCK, new Identifier("enderthyst", "enderthyst_ore"), ENDERTHYST_ORE);
+		Registry.register(Registry.ITEM, new Identifier("enderthyst", "enderthyst_ore"), new BlockItem(ENDERTHYST_ORE, new Item.Settings().group(ItemGroup.BUILDING_BLOCKS)));
+
+		RegistryKey<ConfiguredFeature<?,?>> enderthystOreEnd = RegistryKey.of(Registry.CONFIGURED_FEATURE_WORLDGEN, new Identifier("enderthyst", "enderthyst_ore_end"));
+		Registry.register(BuiltinRegistries.CONFIGURED_FEATURE, enderthystOreEnd.getValue(), ENDERTHYST_ORE_END);
+		BiomeModifications.addFeature(BiomeSelectors.foundInTheEnd(), GenerationStep.Feature.UNDERGROUND_ORES, enderthystOreEnd);
 	}
 }
---
+++ src/main/java/fireblaze/ender/ore/EnderthystOreBlock.java
@@ -0,0 +1,10 @@
+package fireblaze.ender.ore;
+
+import net.minecraft.block.Block;
+    public class EnderthystOreBlock extends Block{
+
+    public EnderthystOreBlock(Settings settings) {
+        super(settings);
+    }
+
+}
