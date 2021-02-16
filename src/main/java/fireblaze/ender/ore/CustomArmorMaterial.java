ea1592b366b135e684292f8ad2f9493be79ae636
Aayaan Sahu <aayaansahu@gmail.com>
2021-02-16 07:32:23 -0800

    Fixed the broken armor

--- src/main/java/fireblaze/ender/ore/CustomArmorMaterial.java
+++ src/main/java/fireblaze/ender/ore/CustomArmorMaterial.java
@@ -1,114 +1,78 @@
-import net.fabricmc.fabric.api.event.client.ClientSpriteRegistryCallback.Registry;
+package fireblaze.ender.ore;
+
+import java.util.function.Supplier;
+
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.item.ArmorMaterial;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemConvertible;
+import net.minecraft.item.Items;
 import net.minecraft.recipe.Ingredient;
+
 import net.minecraft.sound.SoundEvent;
 import net.minecraft.sound.SoundEvents;
 
-public class CustomArmorMaterial implements ArmorMaterial {
+import net.minecraft.util.Lazy;
+
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+// Make sure it is an enum
+public enum CustomArmorMaterial implements ArmorMaterial {
+
+    // I honestly don't know what type of syntax this is... It's supposed to be an enum ¯\_(ツ)_/¯
+     ENDERTHYST("enderthyst", 40, new int[] {6, 9, 11, 6}, 30, SoundEvents.ITEM_ARMOR_EQUIP_NETHERITE, 1.0F, () -> {
+         return Ingredient.ofItems(Items.GUNPOWDER);
+     });
+
+    // Base durability is multiplied by the durabilityMultiplier, I have no idea why
     private static final int[] BASE_DURABILITY = new int[] { 13, 15, 16, 11 };
-    private static final int A = 0;
-    private static final int B = 0;
-    private static final int C = 0;
-    private static final int D = 0;
-    private static final int[] PROTECTION_VALUES = new int[] { A, B, C, D };
 
+    private final String name;
+    private final int durabilityMultiplier;
+    private final int[] protectionAmounts;
+    private final int enchantability;
+    private final SoundEvent equipSound;
+    private final float toughness;
+    private final Lazy<Ingredient> repairIngredientSupplier;
+
+
+    // Constructor function
+    CustomArmorMaterial(String string_1, int int_1, int[] ints_1, int int_2, SoundEvent soundEvent_1, float float_1, Supplier<Ingredient> supplier_1) {
+        this.name = string_1;
+        this.durabilityMultiplier = int_1;
+        this.protectionAmounts = ints_1;
+        this.enchantability = int_2;
+        this.equipSound = soundEvent_1;
+        this.toughness = float_1;
+        this.repairIngredientSupplier = new Lazy(supplier_1);
+    }
+
+    // Must implememnt these since we are implementing ArmorMaterial
     @Override
     public int getDurability(EquipmentSlot slot) {
-        return BASE_DURABILITY[slot.getEntitySlotId()] * 69;
+        return BASE_DURABILITY[slot.getEntitySlotId()] * this.durabilityMultiplier;
     }
 
     @Override
     public int getProtectionAmount(EquipmentSlot slot) {
-        return PROTECTION_VALUES[slot.getEntitySlotId()];
+        return this.protectionAmounts[slot.getEntitySlotId()];
     }
 
     @Override
-    public int getEnchantability() {
-        return 20;
-    }
+    public int getEnchantability() { return this.enchantability; }
 
     @Override
-    public SoundEvent getEquipSound() {
-        return SoundEvents.ITEM_ARMOR_EQUIP_Netherite;
-    }
+    public SoundEvent getEquipSound() { return this.equipSound; }
 
     @Override
-    public Ingredient getRepairIngredient() {
-        return Ingredient.ofItems(RegisterItems.netherite);
-    }
+    public Ingredient getRepairIngredient() { return this.repairIngredientSupplier.get(); }
 
+    @Environment(EnvType.CLIENT)
+    public String getName() { return this.name; }
+
+    public float getToughness() { return this.toughness; }
+
+    // Knockback succ
     @Override
-    public String getName() {
-        return "enderthyst";
-    }
+    public float getKnockbackResistance() { return 0; }
 
-    @Override
-    public float getToughness() {
-        return 4;
-    }
-
-    @Override
-    public float getKnockbackResistance() {
-        return (float) 0.75;
-    }
-
-    @Override
-    public int getDurability(EquipmentSlot slot) {
-        // TODO Auto-generated method stub
-        return 0;
-    }
-
-    @Override
-    public SoundEvent getEquipSound() {
-        // TODO Auto-generated method stub
-        return null;
-    }
-
-    @Override
-    public int getProtectionAmount(EquipmentSlot slot) {
-        // TODO Auto-generated method stub
-        return 0;
-    }
-
-    @Override
-    public Ingredient getRepairIngredient() {
-        // TODO Auto-generated method stub
-        return null;
-    }
-}
-
-// Probably a ton of bugs in the next few lines
-public class RegisterItems {
-
-    public static ItemConvertible netherite;
-    public static final ArmorMaterial enderthystArmor = new CustomArmorMaterial();
-    public static final Item ENDERTHYST = new CustomMaterialItem(new Item.Settings().group(name.NAME)); //Possibly buggy due to name having spaces+apostrophes
-     // If you made a new material, this is where you would note it.
-     public static final Item ENDERTHYST_HELMET = new ArmorItem(enderthystArmor, EquipmentSlot.HEAD, new Item.Settings().group(name.NAME));
-     public static final Item ENDERTHYST_CHESTPLATE = new ArmorItem(enderthystArmor, EquipmentSlot.CHEST, new Item.Settings().group(name.NAME));
-     public static final Item ENDERTHYST_LEGGINGS = new ArmorItem(enderthystArmor, EquipmentSlot.LEGS, new Item.Settings().group(name.NAME));
-     public static final Item ENDERTHYST_BOOTS = new ArmorItem(enderthystArmor, EquipmentSlot.FEET, new Item.Settings().group(name.NAME));
-  
- }
-
- //variable names might need changing
- public static void register() {
-    Registry.register(Registry.ITEM, new Identifier("name", "enderthyst"), ENDERTHYST);
-    Registry.register(Registry.ITEM, new Identifier("name", "enderthyst_helmet"), ENDERTHYST_HELMET);
-    Registry.register(Registry.ITEM, new Identifier("name", "enderthyst_chestplate"), ENDERTHYST_CHESTPLATE);
-    Registry.register(Registry.ITEM, new Identifier("name", "enderthyst_leggings"), ENDERTHYST_LEGGINGS);
-    Registry.register(Registry.ITEM, new Identifier("name", "enderthyst_boots"), ENDERTHYST_BOOTS);
-}
-
-public static final ItemGroup EXAMPLE_MOD_GROUP = FabricItemGroupBuilder.create(
-    new Identifier("armor", "endermod")) //Possibly buggy since names might need to match variables
-    .icon(() -> new ItemStack(RegisterItems.ENDERTHYST)) // This uses the model of the new material you created as an icon, but you can reference to whatever you like
-    .build();
-
-@Override
-public void onInitialize() {
-RegisterItems.register();
 }
--- src/main/java/fireblaze/ender/ore/EnderOre.java
+++ src/main/java/fireblaze/ender/ore/EnderOre.java
@@ -1,36 +1,56 @@
 package fireblaze.ender.ore;
 
 import net.fabricmc.api.ModInitializer;
+
 import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
 import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
+
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
 import net.minecraft.item.BlockItem;
+import net.minecraft.structure.rule.BlockMatchRuleTest;
+
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
-import net.minecraft.structure.rule.BlockMatchRuleTest;
+
 import net.minecraft.util.Identifier;
+
 import net.minecraft.util.registry.BuiltinRegistries;
 import net.minecraft.util.registry.Registry;
 import net.minecraft.util.registry.RegistryKey;
+
 import net.minecraft.world.gen.decorator.Decorator;
 import net.minecraft.world.gen.decorator.RangeDecoratorConfig;
-import net.minecraft.world.gen.feature.ConfiguredFeature;
+
 import net.minecraft.world.gen.feature.Feature;
+import net.minecraft.world.gen.feature.ConfiguredFeature;
 import net.minecraft.world.gen.feature.OreFeatureConfig;
+
 import net.minecraft.world.gen.GenerationStep;
+
+import net.minecraft.item.ArmorItem;
+import net.minecraft.entity.EquipmentSlot;
+
+
 public class EnderOre implements ModInitializer {
 
 	public static final Item ENDERTHYST_SHARD = new Item(new Item.Settings().group(ItemGroup.MATERIALS));
 	public static final Block ENDERTHYST_ORE = new EnderthystOreBlock(FabricBlockSettings.copy(Blocks.ANCIENT_DEBRIS));
 	private static ConfiguredFeature<?,?> ENDERTHYST_ORE_END = Feature.ORE.configure(new OreFeatureConfig(new BlockMatchRuleTest(Blocks.END_STONE), ENDERTHYST_ORE.getDefaultState(),9)).decorate(Decorator.RANGE.configure(new RangeDecoratorConfig(0,0,240))).spreadHorizontally().repeat(20);
 
+	// Armor Variables
+	public static final Item ENDERTHYST_HELMET     = new ArmorItem(CustomArmorMaterial.ENDERTHYST,  EquipmentSlot.HEAD, new Item.Settings().group(ItemGroup.COMBAT));
+	public static final Item ENDERTHYST_CHESTPLATE = new ArmorItem(CustomArmorMaterial.ENDERTHYST, EquipmentSlot.CHEST, new Item.Settings().group(ItemGroup.COMBAT));
+	public static final Item ENDERTHYST_LEGGINGS   = new ArmorItem(CustomArmorMaterial.ENDERTHYST,  EquipmentSlot.LEGS, new Item.Settings().group(ItemGroup.COMBAT));
+	public static final Item ENDERTHYST_BOOTS      = new ArmorItem(CustomArmorMaterial.ENDERTHYST,  EquipmentSlot.FEET, new Item.Settings().group(ItemGroup.COMBAT));
+
 
 	@Override
 	public void onInitialize() {
 
-		Registry.register(Registry.ITEM,new Identifier("enderthyst","enderthyst_shard"),ENDERTHYST_SHARD);
+		Registry.register(Registry.ITEM,new Identifier("enderthyst","enderthyst_shard"), ENDERTHYST_SHARD);
 
 		/** The enderthyst tools */
 
@@ -50,17 +70,27 @@ public class EnderOre implements ModInitializer {
 		final float shovelAttackSpeed  =  -2.0F;
 		final float hoeAttackSpeed     =  -2.0F;
 
+		// Register the tools
 		Registry.register(Registry.ITEM, new Identifier("enderthyst", "enderthyst_pickaxe"), new EnderthystPickaxe(new EnderthystToolMaterial(), pickaxeAttackDamage, pickaxeAttackSpeed, new Item.Settings().group(ItemGroup.TOOLS)));
 		Registry.register(Registry.ITEM, new Identifier("enderthyst", "enderthyst_axe"),     new     EnderthystAxe(new EnderthystToolMaterial(),     axeAttackDamage,     axeAttackSpeed, new Item.Settings().group(ItemGroup.TOOLS)));
 		Registry.register(Registry.ITEM, new Identifier("enderthyst", "enderthyst_sword"),   new   EnderthystSword(new EnderthystToolMaterial(),   swordAttackDamage,   swordAttackSpeed, new Item.Settings().group(ItemGroup.TOOLS)));
 		Registry.register(Registry.ITEM, new Identifier("enderthyst", "enderthyst_shovel"),  new  EnderthystShovel(new EnderthystToolMaterial(),  shovelAttackDamage,  shovelAttackSpeed, new Item.Settings().group(ItemGroup.TOOLS)));
 		Registry.register(Registry.ITEM, new Identifier("enderthyst", "enderthyst_hoe"),     new     EnderthystHoe(new EnderthystToolMaterial(),     hoeAttackDamage,     hoeAttackSpeed, new Item.Settings().group(ItemGroup.TOOLS)));
 
+		
+		// Register the armor
+        Registry.register(Registry.ITEM, new Identifier("enderthyst",       "enderthyst_helmet"),   ENDERTHYST_HELMET);
+        Registry.register(Registry.ITEM, new Identifier("enderthyst",   "enderthyst_chestplate"),   ENDERTHYST_CHESTPLATE);
+        Registry.register(Registry.ITEM, new Identifier("enderthyst",     "enderthyst_leggings"),   ENDERTHYST_LEGGINGS);
+        Registry.register(Registry.ITEM, new Identifier("enderthyst",        "enderthyst_boots"),   ENDERTHYST_BOOTS);
+		
+		// Register the block
 		Registry.register(Registry.BLOCK, new Identifier("enderthyst", "enderthyst_ore"), ENDERTHYST_ORE);
 		Registry.register(Registry.ITEM, new Identifier("enderthyst", "enderthyst_ore"), new BlockItem(ENDERTHYST_ORE, new Item.Settings().group(ItemGroup.BUILDING_BLOCKS)));
 
 		RegistryKey<ConfiguredFeature<?,?>> enderthystOreEnd = RegistryKey.of(Registry.CONFIGURED_FEATURE_WORLDGEN, new Identifier("enderthyst", "enderthyst_ore_end"));
 		Registry.register(BuiltinRegistries.CONFIGURED_FEATURE, enderthystOreEnd.getValue(), ENDERTHYST_ORE_END);
 		BiomeModifications.addFeature(BiomeSelectors.foundInTheEnd(), GenerationStep.Feature.UNDERGROUND_ORES, enderthystOreEnd);
+
 	}
 }
