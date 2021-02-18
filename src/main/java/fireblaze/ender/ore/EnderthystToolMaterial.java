142a78782f76e651ea10376d32888baedbf0b0fa
Aayaan Sahu <aayaansahu@gmail.com>
2021-02-18 11:32:28 -0800

    Added acid bucket texture

--- src/main/java/fireblaze/ender/ore/EnderthystToolMaterial.java
+++ src/main/java/fireblaze/ender/ore/EnderthystToolMaterial.java
@@ -8,29 +8,36 @@ public class EnderthystToolMaterial implements ToolMaterial {
 
     public static final EnderthystToolMaterial INSTANCE = new EnderthystToolMaterial();
 
+    private int durability = 3065;
+    private float miningSpeedMultiplier = 10.0F;
+    public float attackDamage = 5.0F;
+    public int miningLevel = 5;
+    public int enchantability = 25;
+    public Ingredient repairIngredient = Ingredient.ofItems(Items.DIAMOND);
+
     @Override
     public int getDurability() {
-        return 3065;
+        return durability;
     }
 
     @Override
     public float getMiningSpeedMultiplier() {
-        return 10.0F;
+        return miningSpeedMultiplier;
     }
 
     @Override
     public float getAttackDamage() {
-        return 5.0F;
+        return attackDamage;
     }
 
     @Override
     public int getMiningLevel() {
-        return 5;
+        return miningLevel;
     }
 
     @Override
     public int getEnchantability() {
-        return 25;
+        return enchantability;
     }
 
     @Override
